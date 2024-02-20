36b3ea6b026eace13b8983adaaaaab711f8409fb
onlyno999 <105115958+onlyno999@users.noreply.github.com>
2024-02-20 22:39:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // 设置优选地址，不带端口号默认8443，不支持非TLS订阅生成
 let addresses = [
-	'icook.tw:2053#台湾',
-	'cloudflare.cfgo.cc#美国'
+	'icook.tw:2053#关注抖音号',
+	'cloudflare.cfgo.cc#onlyno999_更新中'
 ];
 
 // 设置优选地址api接口
